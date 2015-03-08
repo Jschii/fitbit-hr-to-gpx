eeee35558c55b4204972635b621bfc28143c61d4
Jussi Martikainen <jmmartik@gmail.com>
2015-03-08 11:22:30 +0200

    Fix ms -> seconds conversion

--- userscript/fitbithr.user.js
+++ userscript/fitbithr.user.js
@@ -6,13 +6,13 @@
 // @include       https://www.fitbit.com/activities/exercise/*
 // ==/UserScript==
 
-this.$ = this.jQuery = jQuery.noConflict(true)
+this.$ = this.jQuery = jQuery.noConflict(true);
 $(document).ready(function() {
-    var scriptData = $('#scripts').html()
+    var scriptData = $('#scripts').html();
     var hrData = scriptData.match(/\{"duration":[0-9]+,"value":([3-9][0-9]|[1-2][0-9]{2})}/g).map(function(hr) {
-        return hr.replace(/duration/g, "d").replace(/value/g, "v").replace(/000/g, "")
-    })
-    var json = '{"h":[' + hrData.join(",") + ']}'
-    $('.chart-sections .sections').append('<section class="chart-section"><div><h2>Copy to clipboard</h2><textarea class="info">' + json + '</textarea></div></section>')
+        return hr.replace(/duration/g, "d").replace(/value/g, "v").replace(/000,/g, ",")
+    });
+    var json = '{"h":[' + hrData.join(",") + ']}';
+    $('.chart-sections .sections').append('<section class="chart-section"><div><h2>Copy to clipboard</h2><textarea class="info">' + json + '</textarea></div></section>');
     //window.prompt("Copy to clipboard", json)
-})
+});
